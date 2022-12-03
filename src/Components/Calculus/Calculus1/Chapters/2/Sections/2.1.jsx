e1c94e448183ba5da15374b67d362fa72ff9a499
Charles Callender <charlescallender.cc@gmail.com>
2022-12-03 15:57:03 -0800

    Added section 2.1 examples 1-2

--- src/Components/Calculus/Calculus1/Chapters/2/Sections/2.1.jsx
+++ src/Components/Calculus/Calculus1/Chapters/2/Sections/2.1.jsx
@@ -43,15 +43,35 @@ export default function Calculus1_Ch2_Sec1() {
             <div className="examples"> 
                 <div className="Example1">
                     <h3>Example 1</h3>
-                    <span>How is the radian measure of an angle determined?</span>
+                    <span><bold>Average Velocity</bold>The position of an object moving vertically along a line is given by the function <MathComponent tex="s(t) = -16t^{2} + 128t" />
+                            Find the average velocity of the object over the following intervals:
+                     </span>
                     
                     <span>
-                        <p>Answer:</p>
-                        The radian measure of an angle <MathComponent tex="\theta" /> is the length s of an arc on the unit circle associated with <MathComponent tex="\theta" />
-                    </span>
+                        <p>a. [1,4]</p>
+                        <MathComponent tex="=\frac{s(4) - s(1)}{4 - 1} = \frac{256 - 112}{3} = 48" /> 
+                        <p>b. [1,3]</p>
+                        <MathComponent tex="=\frac{s(3) - s(1)}{3 - 1} = \frac{240 - 112}{2} = 64" /> 
+                        <p>c. [1,2]</p> 
+                        <MathComponent tex="=\frac{s(2) - s(1)}{2 - 1} = \frac{192 - 112}{1} = 80" /> 
+                    </span> *10
                 </div>
 
-             
+                <div className="Example2">
+                    <h3>Example 2</h3>
+                    <span><bold>Instantaneous Velocity</bold>Consider the position function <MathComponent tex="s(t) = -16t^{2} + 100t" />
+                            Complete the following table with the appropriate average velocities. Then make a conjecture about the value of the instantaneaous velocity at t = 3
+                     </span>
+                    
+                    <span>
+                        
+                        <MathComponent tex="\begin{array} {|r|r|}\hline Time Interval & Average Velocity \\ \hline [2,3] &  \\ \hline [2.9,3] &  \\ \hline [2.99,3] &  \\ \hline [2.999,3] &  \\ \hline [2.999,3] &  \\ \hline  \end{array}" /> 
+                        <p>Answer:</p>
+                        <MathComponent tex="\begin{array} {|r|r|}\hline Time Interval & Average Velocity \\ \hline [2,3] & 20 \\ \hline [2.9,3] & 5.6 \\ \hline [2.99,3] & 4.16 \\ \hline [2.999,3] & 4.016 \\ \hline [2.999,3] & 4.002 \\ \hline  \end{array}" />
+
+                        <MathComponent tex="v_{inst} = 4" /> 
+                    </span> *11
+                </div>           
                
 
                 
