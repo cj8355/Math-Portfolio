df6c634adc475a004a661912e9262d2dcf803914
Charles Callender <charlescallender.cc@gmail.com>
2022-12-06 22:28:21 -0800

    Added section 2.1 ex 1

--- src/Components/Calculus/Calculus1/Chapters/2/Sections/2.2.jsx
+++ src/Components/Calculus/Calculus1/Chapters/2/Sections/2.2.jsx
@@ -52,18 +52,21 @@ export default function Calculus1_Ch2_Sec2() {
             <div className="examples"> 
                 <div className="Example1">
                     <h3>Example 1</h3>
-                    <span><bold>Average Velocity</bold>The position of an object moving vertically along a line is given by the function <MathComponent tex="s(t) = -16t^{2} + 128t" />
-                            Find the average velocity of the object over the following intervals:
-                     </span>
                     
+
                     <span>
-                        <p>a. [1,4]</p>
-                        <MathComponent tex="=\frac{s(4) - s(1)}{4 - 1} = \frac{256 - 112}{3} = 48" /> 
-                        <p>b. [1,3]</p>
-                        <MathComponent tex="=\frac{s(3) - s(1)}{3 - 1} = \frac{240 - 112}{2} = 64" /> 
-                        <p>c. [1,2]</p> 
-                        <MathComponent tex="=\frac{s(2) - s(1)}{2 - 1} = \frac{192 - 112}{1} = 80" /> 
-                    </span> *10
+                        Let
+                        <MathComponent tex="f(x) = \frac{x^{2} - 4}{x - 2}" /> 
+                        <p>a. Calculate f(x) for each value of x in the following table.</p>
+                        
+                        <p>b. Make a conjecture about the value of </p> 
+                        <MathComponent tex="\lim_{x\to 2} \frac{x^{2} - 4}{x - 2}" />
+
+                        <MathComponent tex="\begin{array} {|r|r|}\hline x & 1.9 & 1.99 & 1.999 & 1.9999 \\ \hline f(x) = \frac{x^{2} - 4}{x - 2} &  &  &  &  \\ \hline x & 2.1 & 2.01 & 2.001 & 2.0001 \\ \hline f(x) = \frac{x^{2} - 4}{x - 2} &  &  &  &  \\ \hline  \end{array}" />
+                        <MathComponent tex="\begin{array} {|r|r|}\hline x & 1.9 & 1.99 & 1.999 & 1.9999 \\ \hline f(x) = \frac{x^{2} - 4}{x - 2} & 3.9 & 3.99 & 3.999 & 3.9999 \\ \hline x & 2.1 & 2.01 & 2.001 & 2.0001 \\ \hline f(x) = \frac{x^{2} - 4}{x - 2} & 4.1 & 4.01 & 4.001 & 4.0001 \\ \hline  \end{array}" />
+
+                        4
+                    </span> *14
                 </div>
 
                  
