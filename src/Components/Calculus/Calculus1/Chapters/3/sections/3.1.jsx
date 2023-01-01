7635df6e4118523c86c2efc4b19ff7c1ec33159f
Charles Callender <charlescallender.cc@gmail.com>
2022-12-31 21:29:14 -0800

    Added 3.1 examples 3,4

--- src/Components/Calculus/Calculus1/Chapters/3/sections/3.1.jsx
+++ src/Components/Calculus/Calculus1/Chapters/3/sections/3.1.jsx
@@ -123,6 +123,54 @@ export default function Calculus1_Ch3_Sec1() {
                     </span> *61
                 </div>
 
+
+                <div className="Example3">
+                    <h3>Example 3</h3>
+                        <p>Find the Function</p>
+                        <p>The following limits represent the slope of a curve <MathComponent tex="y = f(x)" /> at the point (a, f(a)).
+                            Determine a possible function f and number a; then calculate the limit.
+                        </p>
+                        
+                        <div>
+                        <MathComponent tex="\lim_{x\to 2} \frac{5x^{2} - 20}{x - 2}" />
+                        
+
+                        <p>Answer:</p>
+                        <p><MathComponent tex="f(x) = 5x^{2}" />
+                        <MathComponent tex="a = 2" />
+                        Limit  = 20
+                        </p>
+                        
+                        </div>
+                    <span>
+                        
+                    </span> *62
+                </div>
+
+
+                <div className="Example4">
+                    <h3>Example 4</h3>
+                        <p>Find the Function</p>
+                        <p>The following limits represent the slope of a curve <MathComponent tex="y = f(x)" /> at the point (a, f(a)).
+                            Determine a possible function f and number a; then calculate the limit.
+                        </p>
+                        
+                        <div>
+                        <MathComponent tex="\lim_{h\to 0} \frac{(2 + h)^{4} - 16}{h}" />
+                        
+
+                        <p>Answer:</p>
+                        <p><MathComponent tex="f(x) = x^{4}" />
+                        <MathComponent tex="a = 2" />
+                        Limit  = 32
+                        </p>
+                        
+                        </div>
+                    <span>
+                        
+                    </span> *63
+                </div>
+
                 
             </div>
             </div>
