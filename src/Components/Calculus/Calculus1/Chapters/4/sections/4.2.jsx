98722a8114ea675e727ceed3823f5bf2f5620980
Charles Callender <charlescallender.cc@gmail.com>
2023-01-21 18:34:25 -0800

    Added 4.2 examples 1-4

--- src/Components/Calculus/Calculus1/Chapters/4/sections/4.2.jsx
+++ src/Components/Calculus/Calculus1/Chapters/4/sections/4.2.jsx
@@ -54,7 +54,101 @@ export default function Calculus1_Ch4_Sec2() {
 
             <h1>Examples</h1>
             <div className="examples"> 
-           
+            <div className="Example1">
+                    <h3>Example 1</h3>
+                      <h4>Rolle's Theorem</h4>
+                      <p>
+                        Determine whether Rolle's Theorem applies to the following functions on the given interval.
+                        If so, find the point(s) guaranteed to exist by Rolle's Theorem.
+                      </p>
+                     
+                     
+                      <MathComponent tex="f(x) = x(x - 1)^{2}" />; [0,1]
+
+                      <p>
+                        Answer:
+                        <MathComponent tex="x = \frac{1}{3}" />
+                      </p>
+
+
+                    *157
+                </div>
+
+
+
+                <div className="Example2">
+                    <h3>Example 2</h3>
+                      <h4>Rolle's Theorem</h4>
+                      <p>
+                        Determine whether Rolle's Theorem applies to the following functions on the given interval.
+                        If so, find the point(s) guaranteed to exist by Rolle's Theorem.
+                      </p>
+                     
+                     
+                      <MathComponent tex="f(x) = 1 - x^{2/3}" />; [-1,1]
+
+                      <p>
+                        Answer:
+                        Rolle's Theorem doesn't apply since
+                        <MathComponent tex="f'(x) = - \frac{2}{3x^{\frac{1}{3}}}" />
+                        doesn't = 0 at any point on the interval.
+                      </p>
+
+
+                    *158
+                </div>
+
+
+                <div className="Example3">
+                    <h3>Example 3</h3>
+                      <h4>Mean Value Theorem</h4>
+                      <p>
+                        Consider the following function on the given interval [a, b].
+                      </p>
+                      <div>
+                        <p>a. Determine whether the Mean Value Theorem applies to the following function on the given interval [a, b].</p>
+                        <p>b. If so, find the point(s) that are guaranteed to exist by the Mean Value Theorem</p>
+                      </div>
+                     
+                     
+                      <MathComponent tex="f(x) = 7 - x^{2}" />; [-1,2]
+
+                      <p>
+                        Answer:
+                        a. Yes
+                        b. <MathComponent tex="c = \frac{1}{2}" />
+                        
+                      </p>
+
+
+                    *159
+                </div>
+
+
+                <div className="Example4">
+                    <h3>Example 4</h3>
+                      <h4>Mean Value Theorem</h4>
+                      <p>
+                        Consider the following function on the given interval [a, b].
+                      </p>
+                      <div>
+                        <p>a. Determine whether the Mean Value Theorem applies to the following function on the given interval [a, b].</p>
+                        <p>b. If so, find the point(s) that are guaranteed to exist by the Mean Value Theorem</p>
+                      </div>
+                     
+                     
+                      <MathComponent tex="f(x) = 2x^{1/3}" />; [-8,8]
+
+                      <p>
+                        Answer:
+                        a. Does not apply
+                        
+                        
+                      </p>
+
+
+                    *160
+                </div>
               
              
                 
