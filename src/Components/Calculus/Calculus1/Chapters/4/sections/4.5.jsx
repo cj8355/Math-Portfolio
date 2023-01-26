a2ee017893a6313ed0ed80450b31750c263914f7
Charles Callender <charlescallender.cc@gmail.com>
2023-01-25 18:22:00 -0800

    Added 4.5 examples 1-3

--- src/Components/Calculus/Calculus1/Chapters/4/sections/4.5.jsx
+++ src/Components/Calculus/Calculus1/Chapters/4/sections/4.5.jsx
@@ -48,18 +48,57 @@ export default function Calculus1_Ch4_Sec5() {
             <div className="examples"> 
             <div className="Example1">
                     <h3>Example 1</h3>
-                      <h4></h4>
-                      <p></p>
+                      <h4>Minimum-perimeter rectangles</h4>
+                      <p>Of all rectangles of area 100, which one has the minimum perimeter.</p>
                       
                      
                       <MathComponent tex="" />
-                      
+                      <p>
+                        Answer: <br/>
+                        Width = length = 10
+                      </p>
 
 
-                    *
+                    *179
                 </div>
 
 
+                <div className="Example2">
+                    <h3>Example 2</h3>
+                      <h4>Minimum distance</h4>
+                      <p>Find the point P on the line y = 3x that is closest to the point (50, 0). What is the least
+                        distance between P and (50,0)?
+                      </p>
+                      
+                     
+                      <p>
+                        Answer: <br/>
+                        (5,15), distance <MathComponent tex="\approx" /> 47.4
+                      </p>
+
+
+                    *180
+                </div>
+
+
+                <div className="Example3">
+                    <h3>Example 3</h3>
+                      <h4>Shortest ladder</h4>
+                      <p>
+                        An 8-ft-tall fence runs parallel to the wall of a house at a distance of 5 ft. Find the length
+                        of the shortest ladder that extends from the ground to the house without touching the fence. Assume the 
+                        vertical wall of the house is 20 ft high and the horizontal ground extends 20 ft from the fence.
+                      </p>
+                      
+                     
+                      <p>
+                        Answer: <br/>
+                        18.2 ft
+                      </p>
+
+
+                    *181
+                </div>
 
 
              
