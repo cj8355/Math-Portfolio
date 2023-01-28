b0a9dd244f55a3137b88e116449c89996b743924
Charles Callender <charlescallender.cc@gmail.com>
2023-01-27 21:00:31 -0800

    Added 4.6 examples 1-4

--- src/Components/Calculus/Calculus1/Chapters/4/sections/4.6.jsx
+++ src/Components/Calculus/Calculus1/Chapters/4/sections/4.6.jsx
@@ -58,18 +58,110 @@ export default function Calculus1_Ch4_Sec6() {
             <div className="examples"> 
             <div className="Example1">
                     <h3>Example 1</h3>
-                      <h4></h4>
+                      <h4>Linear Approximation</h4>
+                      <div>
+                        <p><b>a.</b>Write the equation of the line that represents the linear approximation to the following
+                          function at the given point a.
+                        </p>
+                        <p><b>b.</b>Use the linear approximation to estimate the given quantity.</p>
+                        <p><b>c.</b>Compute the percent error in your approximation, 100 |approximation - exact|/|exact|,
+                          where the exact value is given by a calculator.
+                        </p>
+                      </div>
                       
                       
-                     
-                      <MathComponent tex="" />
-                    
+                     <p>
+                     <MathComponent tex="f(x) = 12 - x^{2}" /> a = 2; f(2.1)
+                     </p>
+                      
+                      <p>
+                        Answer:<br/>
+                        a. <MathComponent tex="L(x) = -4x + 16" />
+                        b. 7.6
+                        c. 0.13% error
+                      </p>
 
-                    *
+                    *185
                 </div>
 
 
+                <div className="Example2">
+                    <h3>Example 2</h3>
+                      <h4>Linear Approximation</h4>
+                      <div>
+                        <p><b>a.</b>Write the equation of the line that represents the linear approximation to the following
+                          function at the given point a.
+                        </p>
+                        <p><b>b.</b>Use the linear approximation to estimate the given quantity.</p>
+                        <p><b>c.</b>Compute the percent error in your approximation, 100 |approximation - exact|/|exact|,
+                          where the exact value is given by a calculator.
+                        </p>
+                      </div>
+                      
+                      
+                     <p>
+                     <MathComponent tex="f(x) = (8 + x)^{-1/3}" /> a = 0; f(-0.1)
+                     </p>
+                      
+                      <p>
+                        Answer:<br/>
+                        a. <MathComponent tex="L(x) = \frac{1}{2} - \frac{x}{48}" />
+                        b. 0.5
+                        c. 0.003% error
+                      </p>
 
+                    *186
+                </div>
+
+
+                <div className="Example3">
+                    <h3>Example 3</h3>
+                      <h4>Differentials</h4>
+                      <div>
+                        <p>Consider the following function and express the relationship between a small change in x and the corresponding change
+                          in y in the form dy = f'(x)dx.
+                        </p>
+                        
+                      </div>
+                      
+                      
+                     <p>
+                     <MathComponent tex="f(x) = 2x + 1" />
+                     </p>
+                      
+                      <p>
+                        Answer:
+                        <MathComponent tex="dy = 2 dx" />
+                        
+                      </p>
+
+                    *187
+                </div>
+
+
+                <div className="Example4">
+                    <h3>Example 4</h3>
+                      <h4>Differentials</h4>
+                      <div>
+                        <p>Consider the following function and express the relationship between a small change in x and the corresponding change
+                          in y in the form dy = f'(x)dx.
+                        </p>
+                        
+                      </div>
+                      
+                      
+                     <p>
+                     <MathComponent tex="f(x) = 3x^{3} - 4x" />
+                     </p>
+                      
+                      <p>
+                        Answer:
+                        <MathComponent tex="dy = (9x^{2} - 4) dx" />
+                        
+                      </p>
+
+                    *188
+                </div>
 
              
                 
