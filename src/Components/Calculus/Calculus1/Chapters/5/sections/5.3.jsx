44765bf08e5a51259efa0d6a6fa15a2451e045e6
Charles Callender <charlescallender.cc@gmail.com>
2023-02-02 14:13:50 -0800

    Added 5.3 examples 1-4

--- src/Components/Calculus/Calculus1/Chapters/5/sections/5.3.jsx
+++ src/Components/Calculus/Calculus1/Chapters/5/sections/5.3.jsx
@@ -48,14 +48,76 @@ export default function Calculus1_Ch5_Sec3() {
             <div className="examples"> 
             <div className="Example1">
                     <h3>Example 1</h3>
-                      <h4></h4>
+                      <h4>Definite Integrals</h4>
+                      <p>Evaluate the following integrals using the Fundamental Theorem of Calculus.</p>
                      
-                      
-                      
-                     <MathComponent tex="" />
+                     <MathComponent tex="\int_{0}^{2} 4x^{3} dx" />
+                     <p>
+                        Answer:<br/>
+                        Antiderivative
+                        <MathComponent tex="x^{4}" />
+                        <MathComponent tex="2^{4} - 0^{4} = 16" />
+                     </p>
 
 
-                    *
+                    *230
+                </div>
+
+
+                <div className="Example2">
+                    <h3>Example 2</h3>
+                      <h4>Definite Integrals</h4>
+                      <p>Evaluate the following integrals using the Fundamental Theorem of Calculus.</p>
+                     
+                     <MathComponent tex="\int_{-2}^{2} (x^{2} - 4) dx" />
+                     <p>
+                        Answer:<br/>
+                        Antiderivative
+                        <MathComponent tex="\frac{1}{3}x^{3} - 4x" />
+                        <MathComponent tex=" (\frac{1}{3}(2^{3} - 4(2)) - (\frac{1}{3}(-2^{3}) - 4(-2))) = (-\frac{16}{3} - \frac{16}{3}) = -\frac{32}{3}" />
+                     </p>
+
+
+                    *231
+                </div>
+
+
+                <div className="Example3">
+                    <h3>Example 3</h3>
+                      <h4>Definite Integrals</h4>
+                      <p>Evaluate the following integrals using the Fundamental Theorem of Calculus.</p>
+                     
+                     <MathComponent tex="\int_{1}^{4} (1 - x)(x - 4) dx" />
+                     <p>
+                        Answer:<br/>
+                        <MathComponent tex="(1 - x)(x - 4) = (-x^{2} + 5x - 4)" />
+                        Antiderivative
+                        <MathComponent tex="- \frac{1}{3}x^{3} + \frac{5}{2}x^{2} - 4x" />
+                        <MathComponent tex="(- \frac{1}{3}(4)^{3} + \frac{5}{2}(4)^{2} - 4(4))-(- \frac{1}{3}(1)^{3} + \frac{5}{2}(1)^{2} - 4(1)) = (-\frac{64}{3} + 40 - 16) - (- \frac{1}{3} + \frac{5}{2} - 4) = \frac{16}{6} + \frac{11}{6} = \frac{9}{2}" />
+                     </p>
+
+
+                    *232
+                </div>
+
+
+                <div className="Example4">
+                    <h3>Example 4</h3>
+                      <h4>Areas of regions</h4>
+                      <p>Find the area of the region bounded by the graph of f and the x-axis on the given interval.</p>
+                     
+                     <MathComponent tex="f(x) = x^{2} - 25" /> <p>on [2,4]</p>
+                     <p>
+                        Answer:<br/>
+                        <MathComponent tex="\int_{2}^{4} x^{2} - 25 dx" />
+                        Antiderivative
+                        <MathComponent tex="\frac{1}{3}x^{3} -25x" />
+                        <MathComponent tex="( \frac{1}{3}(4)^{3} - 25(4))-(- \frac{1}{3}(2)^{3} - 25(2)) = (\frac{64}{3} - 100) - (\frac{8}{3} - 50) = -\frac{236}{3} + \frac{142}{3} = - \frac{94}{3}" />
+                        Take the absolute value of <MathComponent tex="-\frac{94}{3} = \frac{94}{3}" /> since area can't be negative.
+                     </p>
+
+
+                    *233
                 </div>
 
 
