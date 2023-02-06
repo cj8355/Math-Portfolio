f6ecff6fb59e7060d2a113c2468117c90e698656
Charles Callender <charlescallender.cc@gmail.com>
2023-02-06 15:30:26 -0800

    Added 6.2 examples 1-4

--- src/Components/Calculus/Calculus2/Chapters/6/sections/6.2.jsx
+++ src/Components/Calculus/Calculus2/Chapters/6/sections/6.2.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import "./6.1.css";
 import { MathComponent } from "mathjax-react";
+import graph1 from "./images/graph1.png";
+import graph2 from "./images/graph2.png";
 
 export default function Calculus2_Ch6_Sec2() {
     
@@ -36,19 +38,19 @@ export default function Calculus2_Ch6_Sec2() {
             <div className="Example-1">
                     <h3>Example 1</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Finding area</h4>
                       <p>
-                        
+                        Determine the area of the shaded region in the following figure.
                       </p>
-                      
+                      <img src={graph1} alt="Graph of function" ></img>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\int_{-1}^{2} x - x^{2} + 2 dx = \frac{9}{2}" />    
                          
                     </p>
 
-                    *
+                    *254
                     </div>
                 </div>
 
@@ -56,19 +58,19 @@ export default function Calculus2_Ch6_Sec2() {
                 <div className="Example-2">
                     <h3>Example 2</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Finding area</h4>
                       <p>
-                        
+                        Determine the area of the shaded region in the following figure.
                       </p>
-                      
+                      <img src={graph2} alt="Graph of function" ></img>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\int_{0}^{2} -2x - x^{2} + 4x dx = \frac{4}{3}" />    
                          
                     </p>
 
-                    *
+                    *255
                     </div>
                 </div>
 
@@ -76,19 +78,19 @@ export default function Calculus2_Ch6_Sec2() {
                 <div className="Example-3">
                     <h3>Example 3</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Regions between curves</h4>
                       <p>
-                        
+                        Find the area of the region describes in the following exercise.
                       </p>
-                      
+                      <p>The region bounded by y = 4x + 4, y = 6x + 6, and x = 4</p>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\int_{-1}^{4} 2x + 2 dx = 25" />    
                          
                     </p>
 
-                    *
+                    *256
                     </div>
                 </div>
 
@@ -96,41 +98,22 @@ export default function Calculus2_Ch6_Sec2() {
                 <div className="Example-4">
                     <h3>Example 4</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Regions between curves</h4>
                       <p>
-                        
+                        Find the area of the region describes in the following exercise.
                       </p>
-                      
+                      <p>The region bounded by <MathComponent tex="y = x^{2} - 2x + 1" /> and <MathComponent tex="y = 5x - 9" /></p>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\int_{2}^{5} -x^{2} + 7x -10 dx = \frac{9}{2}" />    
                          
                     </p>
 
-                    *
+                    *257
                     </div>
                 </div>
 
-
-                <div className="Example-5">
-                    <h3>Example 5</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
 
 
 
