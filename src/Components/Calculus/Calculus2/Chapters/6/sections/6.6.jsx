d34c1c15e4efd183fd6307ec29cf7ce1de985ee3
Charles Callender <charlescallender.cc@gmail.com>
2023-02-10 13:24:27 -0800

    Added 6.6 examples 1-3

--- src/Components/Calculus/Calculus2/Chapters/6/sections/6.6.jsx
+++ src/Components/Calculus/Calculus2/Chapters/6/sections/6.6.jsx
@@ -29,26 +29,40 @@ export default function Calculus2_Ch6_Sec6() {
             <div className="examples"> 
             <div className="Example-1">
                     <h3>Example 1</h3>
-                    
+                    <div className="textBorder">
+                      <h4>Computing surface areas</h4>
+                      <p>
+                        Find the area of the surface generated when the given curve is revolved about the given axis.
+                      </p>
+                      <p>y = 3x + 4, for about the <MathComponent tex="0 \leq x \leq 6" />;x-axis</p>
+                     
+                    <p>
+                        Answer: <br/>
+                         <MathComponent tex="S = \int_{0}^{6} 2 \pi (3x+4) \sqrt{1 + 9} dx = 156 \sqrt{10} \pi" />    
+                         
+                    </p>
+
+                    *276
+                    </div>
                 </div>
 
 
                 <div className="Example-2">
                     <h3>Example 2</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Computing surface areas</h4>
                       <p>
-                        
+                        Find the area of the surface generated when the given curve is revolved about the given axis.
                       </p>
-                      
+                      <p><MathComponent tex="y = 8 \sqrt{x}" />, for about the <MathComponent tex="9 \leq x \leq 20" />;x-axis</p>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="S = \int_{0}^{\frac{9}{20}} 2 \pi 8 \sqrt{x} \sqrt{1 + \frac{16}{x}} dx = \frac{2912 \pi}{3}" />    
                          
                     </p>
 
-                    *
+                    *277
                     </div>
                 </div>
 
@@ -56,62 +70,21 @@ export default function Calculus2_Ch6_Sec6() {
                 <div className="Example-3">
                     <h3>Example 3</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Computing surface areas</h4>
                       <p>
-                        
+                        Find the area of the surface generated when the given curve is revolved about the given axis.
                       </p>
-                      
+                      <p><MathComponent tex="x = \sqrt{12y - y^{2}}" />, for about the <MathComponent tex="2 \leq y \leq 10" />;y-axis</p>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="S = \int_{2}^{10} 2 \pi \sqrt{12y-y^{2}} \sqrt{1 + \frac{(6-y)^{2}}{y(12-y)}} dy = 96 \pi" />    
                          
                     </p>
 
-                    *
+                    *278
                     </div>
                 </div>
-
-
-                <div className="Example-4">
-                    <h3>Example 4</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
-
-                <div className="Example-5">
-                    <h3>Example 5</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
 
 
 
