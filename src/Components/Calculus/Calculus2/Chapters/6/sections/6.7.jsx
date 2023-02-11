fbab11d7dab2200a24339feab0f24a47c896f242
Charles Callender <charlescallender.cc@gmail.com>
2023-02-11 11:58:39 -0800

    Added 6.7 examples 1-4

--- src/Components/Calculus/Calculus2/Chapters/6/sections/6.7.jsx
+++ src/Components/Calculus/Calculus2/Chapters/6/sections/6.7.jsx
@@ -74,19 +74,19 @@ export default function Calculus2_Ch6_Sec7() {
             <div className="Example-1">
                     <h3>Example 1</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Mass of one-dimensional objects</h4>
                       <p>
-                        
+                        Find the mass of the following thin bars with the given density function.
                       </p>
-                      
+                      <MathComponent tex="\rho(x) = 1 + sin x, \hspace{.2cm} for \hspace{.2cm} 0 \leq x \leq \pi" /> 
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\int_{0}^{\pi} 1 + sin x dx = \pi + 2" />    
                          
                     </p>
 
-                    *
+                    *283
                     </div>
                 </div>
 
@@ -94,19 +94,20 @@ export default function Calculus2_Ch6_Sec7() {
                 <div className="Example-2">
                     <h3>Example 2</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Mass of one-dimensional objects</h4>
                       <p>
-                        
+                        Find the mass of the following thin bars with the given density function.
                       </p>
-                      
+                      <MathComponent tex="\rho(x) = 2 - \frac{x}{2}, \hspace{.2cm} for \hspace{.2cm} 0 \leq x \leq 2" /> 
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\int_{0}^{2} 2 - \frac{x}{2} dx = 3" />    
                          
                     </p>
 
-                    *
+
+                    *284
                     </div>
                 </div>
 
@@ -114,19 +115,20 @@ export default function Calculus2_Ch6_Sec7() {
                 <div className="Example-3">
                     <h3>Example 3</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Work from Force</h4>
                       <p>
-                        
+                        How much work is required to move an object from x = 0 to x = 3(measured in meters) in the presence
+                        of a force (in N) given by F(x) = 2x acting along the x-axis?
                       </p>
                       
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="W = \int_{0}^{3} 2x dx = 9J" />    
                          
                     </p>
 
-                    *
+                    *285
                     </div>
                 </div>
 
@@ -134,45 +136,29 @@ export default function Calculus2_Ch6_Sec7() {
                 <div className="Example-4">
                     <h3>Example 4</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Emptying a swimming pool</h4>
                       <p>
-                        
+                        A swimming pool has the shape of a box with a base that measures 25m by 15m and a uniform depth of 2.5m.
+                        How much work is required to pump the water out of the pool (to the level of the top of the pool) when it is full?
                       </p>
-                      
+                      <ul className="textList">
+                        <li>Density of Water = 1000 kg/m^3</li>
+                        <li>Gravity = 9.8 m/s^2</li>
+                        <li>A(y) = 25 * 15</li>
+                      </ul>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="W = 1000 \cdot 9.8 \int_{0}^{2.5}  (25*15)(2.5 - y) dy = 1.15 x 10^{7} J" />    
                          
                     </p>
 
-                    *
+                    *286
                     </div>
                 </div>
 
 
-                <div className="Example-5">
-                    <h3>Example 5</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
-
-
-
+               
             </div>
             </div>
            
