eaaac62fb5bfc70722e66cbea550e9a3b32e9bfa
Charles Callender <charlescallender.cc@gmail.com>
2023-02-13 12:21:06 -0800

    Added 7.2 examples 1-3

--- src/Components/Calculus/Calculus2/Chapters/7/sections/7.2.jsx
+++ src/Components/Calculus/Calculus2/Chapters/7/sections/7.2.jsx
@@ -57,19 +57,25 @@ export default function Calculus2_Ch7_Sec2() {
             <div className="Example-1">
                     <h3>Example 1</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Designing exponential growth functions</h4>
+                     <ol>
+                        <li><b>a. </b>Find the rate constant k and use it to devise an exponential growth function that fits the given data</li>
+                        <li><b>b. </b> Answer the accompanying questions.</li>
+                     </ol>
                       <p>
-                        
+                        <b>Population</b><br/>
+                        The population of a town with a 2016 population of 90,000 grows at a rate of 2.4%/yr. In what year will
+                        the population reach 120,000?
                       </p>
-                      
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
-                         
+                         a. <MathComponent tex="ln 1.024 \apprx 0.02372;" />
+                         <MathComponent tex="y(t) = 90,000e^{tln1.024}" />  
+                         b. 2028
                     </p>
 
-                    *
+                    *299
                     </div>
                 </div>
 
@@ -77,19 +83,20 @@ export default function Calculus2_Ch7_Sec2() {
                 <div className="Example-2">
                     <h3>Example 2</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Population of Texas</h4>
+                     
                       <p>
-                        
+                        Texas was the third fastest growing state in the United States in 2016. Texas grew from 25.1 million
+                        in 2010 to 26.47 million in 2016. Use an exponential growth model to predict the population of Texas in 2025.
                       </p>
-                      
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                        28.7 million
                          
                     </p>
 
-                    *
+                    *300
                     </div>
                 </div>
 
@@ -97,63 +104,27 @@ export default function Calculus2_Ch7_Sec2() {
                 <div className="Example-3">
                     <h3>Example 3</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Caffeine</h4>
                       <p>
-                        
+                        After an individual drinks a beverage containing caffeine, the amount of caffeine in the bloodstream
+                        can be modeled by an exponential decay function, with a half-life that depends on several factors, including
+                        age and body weight. For the sake of simplicity, assume the caffeine in the following drink immediately 
+                        the bloodstream upon consumption.
                       </p>
-                      
+                      <p>
+                        An individual consumes an energy drink that contains caffeine. If 80% of the caffeine from the energy
+                        drink is still in the bloodstream 2 hours later, find the half-life of caffeine for this individual.
+                      </p>
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
-                         
+                         <MathComponent tex="k = - \frac{ln(4/5)}{2}" />    
+                         <MathComponent tex="T_{1/2} = \frac{2ln(1/2)}{ln(4/5)} = 6.2 hrs" /> 
                     </p>
 
-                    *
+                    *301
                     </div>
                 </div>
-
-
-                <div className="Example-4">
-                    <h3>Example 4</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
-
-                <div className="Example-5">
-                    <h3>Example 5</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
-
 
 
             </div>
