76339457d64e4b52928fc1b721145c717cc7cc21
Charles Callender <charlescallender.cc@gmail.com>
2023-02-17 10:08:58 -0800

    Added 8.2 examples 1-4

--- src/Components/Calculus/Calculus2/Chapters/8/sections/8.2.jsx
+++ src/Components/Calculus/Calculus2/Chapters/8/sections/8.2.jsx
@@ -50,19 +50,22 @@ export default function Calculus2_Ch8_Sec2() {
             <div className="Example-1">
                     <h3>Example 1</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Integration by parts</h4>
                       <p>
-                        
+                        Evaluate the following integrals using integration by parts.
                       </p>
-                      
+                      <MathComponent tex="\int x \hspace{.1cm} cos \hspace{.1cm} 5x \hspace{.1cm} dx" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="u = x, \hspace{.1cm} dv = cos(5x)" />
+                         <MathComponent tex="= x(\frac{1}{5} sin (5x)) - \int \frac{1}{5} sin (5x) \hspace{.1cm} dx" />
+                         <MathComponent tex="= \frac{1}{5} x sin(5x) \hspace{.1cm} - \hspace{.1cm} \frac{1}{5} \int sin(5x) \hspace{.1cm} dx" />
+                         <MathComponent tex=" = \frac{1}{5} x sin(5x) \hspace{.1cm} + \hspace{.1cm} \frac{1}{25} cos(5x) \hspace{.1cm} + \hspace{.1cm} C" />    
                          
                     </p>
 
-                    *
+                    *321
                     </div>
                 </div>
 
@@ -70,19 +73,22 @@ export default function Calculus2_Ch8_Sec2() {
                 <div className="Example-2">
                     <h3>Example 2</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Integration by parts</h4>
                       <p>
-                        
+                        Evaluate the following integrals using integration by parts.
                       </p>
-                      
+                      <MathComponent tex="\int x \hspace{.1cm} 3^{x} \hspace{.1cm} dx" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="u = x, \hspace{.1cm} dv = 3^{x}" />
+                         <MathComponent tex="= x(\frac{1}{ln(3)} \cdot 3^{x}) - \int \frac{1}{ln(3)} \cdot 3^{x} \hspace{.1cm} dx" />
+                         <MathComponent tex="= \frac{x \cdot 3^{x}}{ln(3)} \hspace{.1cm} - \hspace{.1cm} \frac{1}{ln(3)} (\frac{3^{x}}{ln(3)} + C)" />
+                         <MathComponent tex=" = \frac{3^{x}}{ln 3} (x - \frac{1}{ln 3}) \hspace{.1cm} + C" />    
                          
                     </p>
 
-                    *
+                    *322
                     </div>
                 </div>
 
@@ -90,19 +96,22 @@ export default function Calculus2_Ch8_Sec2() {
                 <div className="Example-3">
                     <h3>Example 3</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Integration by parts</h4>
                       <p>
-                        
+                        Evaluate the following integrals using integration by parts.
                       </p>
-                      
+                      <MathComponent tex="\int e^{x} \hspace{.1cm} cos \hspace{.1cm} x \hspace{.1cm} dx" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="u = cos \hspace{.1cm} x, \hspace{.1cm} dv = e^{x}" />
+                         <MathComponent tex="= cos(x) e^{x} - \int e^{x} (-sin(x)) \hspace{.1cm} dx" />
+                         <MathComponent tex="= cos(x) \hspace{.1cm} e^{x} + sin(x) \hspace{.1cm} e^{x} \hspace{.1cm} - \hspace{.1cm} \int e^{x} \hspace{.1cm} cos(x) \hspace{.1cm} dx" />
+                         <MathComponent tex=" = \frac{e^{x}}{2} (sin \hspace{.1cm} x + cos \hspace{.1cm} x) \hspace{.1cm} + C" />    
                          
                     </p>
 
-                    *
+                    *323
                     </div>
                 </div>
 
@@ -110,42 +119,24 @@ export default function Calculus2_Ch8_Sec2() {
                 <div className="Example-4">
                     <h3>Example 4</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Integration by parts</h4>
                       <p>
-                        
+                        Evaluate the following integrals using integration by parts.
                       </p>
-                      
+                      <MathComponent tex="\int_{0}^{\pi} x \hspace{.1cm} sin \hspace{.1cm} x \hspace{.1cm} dx" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="u = x, \hspace{.1cm} dv = sin(x)" />
+                         <MathComponent tex="= x(-cos(x)) \bigg\vert_{0}^{\pi} \hspace{.1cm} - \hspace{.1cm} \int_{0}^{\pi} - cos(x) \hspace{.1cm} dx" />
+                         <MathComponent tex="= x(-cos(x)) \bigg\vert_{0}^{\pi} \hspace{.1cm} + \hspace{.1cm} sin(x) \bigg\vert_{0}^{\pi}" />
+                         <MathComponent tex=" = \pi \hspace{.1cm} + \hspace{.1cm} sin(\pi) = \hspace{.1cm} \pi" />    
                          
                     </p>
 
-                    *
+                    *324
                     </div>
                 </div>
-
-
-                <div className="Example-5">
-                    <h3>Example 5</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
 
 
 
