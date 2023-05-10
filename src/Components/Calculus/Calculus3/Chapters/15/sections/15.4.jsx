6602182612fb5a6cf2e9a3d530488659b32f4529
Charles Callender <charlescallender.cc@gmail.com>
2023-05-09 17:11:58 -0700

    Added 15.4 examples 1-4

--- src/Components/Calculus/Calculus3/Chapters/15/sections/15.4.jsx
+++ src/Components/Calculus/Calculus3/Chapters/15/sections/15.4.jsx
@@ -52,19 +52,19 @@ export default function Calculus3_Ch15_Sec4() {
             <div className="Example-1">
                     <h3>Example 1</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Chain Rule with one independent variable</h4>
                       <p>
-                        
+                        Use the Chain Rule to find the following derivatives.
                       </p>
-                      
+                      <MathComponent tex="\mathrm{dz/dt, \hspace{.2cm} where \hspace{.2cm} z = x \hspace{.2cm} sin \hspace{.2cm} y, \hspace{.2cm} x = t^{2}, \hspace{.2cm} and \hspace{.2cm} y = 4t^{3}}" /> 
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\mathrm{z'(t) = 2t \hspace{.1cm} sin \hspace{.1cm} 4t^{3} + 12t^{4} \hspace{.1cm} cos \hspace{.1cm} 4t^{3}}" />    
                          
                     </p>
 
-                    *
+                    *562
                     </div>
                 </div>
 
@@ -72,19 +72,19 @@ export default function Calculus3_Ch15_Sec4() {
                 <div className="Example-2">
                     <h3>Example 2</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Chain Rule with one independent variable</h4>
                       <p>
-                        
+                        Use the Chain Rule to find the following derivatives.
                       </p>
-                      
+                      <MathComponent tex="\mathrm{dV/dt, \hspace{.2cm} where \hspace{.2cm} V = xyz \hspace{.2cm}, \hspace{.2cm} x = e^{t}, \hspace{.2cm} y = 2t+3, \hspace{.2cm} and \hspace{.2cm} z = sin \hspace{.1cm} t}" /> 
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\mathrm{V'(t) = e^{t}((2t+5) \hspace{.1cm} sin \hspace{.1cm} t + (2t+3) \hspace{.1cm} cos \hspace{.1cm} t)}" />    
                          
                     </p>
 
-                    *
+                    *563
                     </div>
                 </div>
 
@@ -92,19 +92,19 @@ export default function Calculus3_Ch15_Sec4() {
                 <div className="Example-3">
                     <h3>Example 3</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Chain Rule with several independent variables</h4>
                       <p>
-                        
+                        Find the following derivatives.
                       </p>
-                      
+                      <MathComponent tex="\mathrm{z_{s} \hspace{.2cm} and \hspace{.2cm} z_{t}, \hspace{.2cm} where \hspace{.2cm} z = x^{2} \hspace{.2cm} sin \hspace{.2cm} y, \hspace{.2cm} x = s-t, \hspace{.2cm} y = t^{2}}" /> 
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\mathrm{z_{s} = 2(s-t) \hspace{.1cm} sin \hspace{.1cm} t^{2}; \hspace{.1cm} z_{t} = 2(s-t)(t(s-t)cos \hspace{.1cm} t^{2} - sin \hspace{.1cm} t^{2}) }" />    
                          
                     </p>
 
-                    *
+                    *564
                     </div>
                 </div>
 
@@ -112,42 +112,21 @@ export default function Calculus3_Ch15_Sec4() {
                 <div className="Example-4">
                     <h3>Example 4</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Chain Rule with several independent variables</h4>
                       <p>
-                        
+                        Find the following derivatives.
                       </p>
-                      
+                      <MathComponent tex="\mathrm{z_{s} \hspace{.2cm} and \hspace{.2cm} z_{t}, \hspace{.2cm} where \hspace{.2cm} z = xy - x^{2}y, \hspace{.2cm} x = s+t, \hspace{.2cm} y = s-t}" /> 
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\mathrm{z_{s} = 2s - 3s^{2} - 2st + t^{2}; \hspace{.1cm} z_{t} = -s^{2} - 2t + 2st + 3t^{2}}" />    
                          
                     </p>
 
-                    *
+                    *565
                     </div>
                 </div>
-
-
-                <div className="Example-5">
-                    <h3>Example 5</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
 
 
 
