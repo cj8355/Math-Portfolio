de8ad672df85ae7aa3cfb1ed116c11a63685ef33
Charles Callender <charlescallender.cc@gmail.com>
2023-05-23 13:23:03 -0700

    Added 16.2 examples 1-4

--- src/Components/Calculus/Calculus3/Chapters/16/sections/16.2.jsx
+++ src/Components/Calculus/Calculus3/Chapters/16/sections/16.2.jsx
@@ -44,19 +44,19 @@ export default function Calculus3_Ch16_Sec2() {
             <div className="Example-1">
                     <h3>Example 1</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                      <h4>Evaluating integrals</h4>
                       <p>
-                        
+                        Evaluate the following integrals.
                       </p>
-                      
+                      <MathComponent tex="\int_{0}^{1} \int_{x}^{1} 6y \hspace{.2cm} dy \hspace{.2cm} dx" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="2" />    
                          
                     </p>
 
-                    *
+                    *613
                     </div>
                 </div>
 
@@ -64,19 +64,19 @@ export default function Calculus3_Ch16_Sec2() {
                 <div className="Example-2">
                     <h3>Example 2</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Evaluating integrals</h4>
                       <p>
-                        
+                        Evaluate the following integrals.
                       </p>
-                      
+                      <MathComponent tex="\int_{0}^{1} \int_{0}^{x} 2e^{x^{2}} \hspace{.2cm} dy \hspace{.2cm} dx" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="e - 1" />    
                          
                     </p>
 
-                    *
+                    *614
                     </div>
                 </div>
 
@@ -84,19 +84,19 @@ export default function Calculus3_Ch16_Sec2() {
                 <div className="Example-3">
                     <h3>Example 3</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Evaluating integrals</h4>
                       <p>
-                        
+                        Evaluate the following integrals.
                       </p>
-                      
+                      <MathComponent tex="\int_{0}^{\pi / 2} \int_{y}^{\pi / 2} 6 \hspace{.2cm} sin \hspace{.2cm} (2x-3) \hspace{.2cm} dx \hspace{.2cm} dy" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="2" />    
                          
                     </p>
 
-                    *
+                    *615
                     </div>
                 </div>
 
@@ -104,42 +104,21 @@ export default function Calculus3_Ch16_Sec2() {
                 <div className="Example-4">
                     <h3>Example 4</h3>
                     <div className="textBorder">
-                      <h4></h4>
+                    <h4>Evaluating integrals</h4>
                       <p>
-                        
+                        Evaluate the following integrals.
                       </p>
-                      
+                      <MathComponent tex="\int_{\pi / 2}^{\pi} \int_{0}^{y^{2}} cos \hspace{.2cm} \frac{x}{y} \hspace{.2cm} dx \hspace{.2cm} dy" />
                      
                     <p>
                         Answer: <br/>
-                         <MathComponent tex="" />    
+                         <MathComponent tex="\pi - 1" />    
                          
                     </p>
 
-                    *
+                    *616
                     </div>
                 </div>
-
-
-                <div className="Example-5">
-                    <h3>Example 5</h3>
-                    <div className="textBorder">
-                      <h4></h4>
-                      <p>
-                        
-                      </p>
-                      
-                     
-                    <p>
-                        Answer: <br/>
-                         <MathComponent tex="" />    
-                         
-                    </p>
-
-                    *
-                    </div>
-                </div>
-
 
 
 
